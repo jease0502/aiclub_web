be039cc065e0fdac34f0803e1b43b9adf7a08413
jease0502 <jease0502@gmail.com>
2020-12-28 16:53:48 +0800

    er and relational picture

--- equipment_lend/models.py
+++ equipment_lend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# class EquipmentInfo(models.Model):
+#     equipment_id = 
+#     etitle = models.CharField(max_length=20)
+#     equipment_quantity = models.CharField(max_length=20)
+#     lend_date = models.DateField()
